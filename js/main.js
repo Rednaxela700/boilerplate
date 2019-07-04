f4000d7cf9189b520194988970967720309d9802
rednaxela700 <ohleg@windowslive.com>
2019-07-04 13:13:19 +0200

    toggle class on click of first nav-link element, doesn't work for the rest

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ const navLink = document.querySelectorAll('.nav-link');
 navLink.addEventListener('click', function () {
     const headerContainer = document.querySelector('.header-container');
     headerContainer.classList.toggle('header-container__active');
-    
+
 });
